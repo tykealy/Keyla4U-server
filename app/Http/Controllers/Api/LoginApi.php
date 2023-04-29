30d8e2a92b847b22039a5ebcca39cae2ce19bd02
tk <tykeaboyloy@gmail.com>
2023-04-29 16:59:50 +0700

    --no-edit

--- app/Http/Controllers/Api/LoginApi.php
+++ app/Http/Controllers/Api/LoginApi.php
@@ -4,19 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\View\View;
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\LoginMail;
 
 class LoginApi extends Controller
 {
     public function store(LoginRequest $request)
     {
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $userEmail = $request->email;
+            Mail::to($userEmail)->send(new LoginMail());
             return auth()->user();
         }
         return response()->json(['error' => 'Unauthorised'], 401);
---
+++ resources/views/mail/register-mail.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+Welcome to Keyla4U
+
+You have successfully created an account.
+
+<x-mail::button :url="''">
+        Button Text
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
